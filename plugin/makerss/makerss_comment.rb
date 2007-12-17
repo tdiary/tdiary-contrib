f70fa081accde8f5ae89b992b81022909c0c21b0
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-17 10:59:00 +0000

    platform/tdiary/plugin/makerss: added license.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@3218 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/makerss/makerss_comment.rb
+++ plugin/makerss/makerss_comment.rb
@@ -1,23 +1,30 @@
+#
+# makerss_comment.rb: extension for makerss plugin.
+#
+# Copyright (C) 2007 by SHIBATA Hiroshi <h-sbt@nifty.com>
+# Distributed under GPL2.
+#
+
 class MakeRssComments < MakeRssFull
 	def title
 		'(comments only)'
 	end
-
+	
 	def item( seq, body, rdfsec )
 		return if rdfsec.section.respond_to?( :body_to_html )
 		super
 	end
-
+	
 	def file
 		f = @conf['makerss.no_comments.file'] || 'comments.rdf'
 		f = 'comments.rdf' if f.length == 0
 		f
 	end
-
+	
 	def write( encoder )
 		super( encoder )
 	end
-
+	
 	def url
 		u = @conf['makerss.no_comments.url'] || "#{@conf.base_url}comments.rdf"
 		u = "#{@conf.base_url}comments.rdf" if u.length == 0
