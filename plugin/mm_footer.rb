b0e835f3de8f7471c86061742c86555128ff20be
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-09 10:53:23 +0000

    platform/tdiary/plugin/mm_footer.rb: rename method name.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@4286 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/mm_footer.rb
+++ plugin/mm_footer.rb
@@ -33,7 +33,7 @@ add_body_leave_proc(Proc.new do |date|
 		if date.strftime('%Y-%m-%d') != Time.now.strftime('%Y-%m-%d')
 		  cache_time = 3600 * 12;
 		end
-		NKF.nkf('-e', mm_memo(rssurl, 50, cache_time, template_head, template_list, template_foot))
+		NKF.nkf('-e', mm_footer(rssurl, 50, cache_time, template_head, template_list, template_foot))
 	 else
 		''
 	 end
@@ -50,22 +50,22 @@ MM_FOOTER_HTTP_HEADER = {
 	"Using RSS parser version is #{::RSS::VERSION}.",
 }
 
-def mm_memo(url, max = 5, cache_time = 3600, \
+def mm_footer(url, max = 5, cache_time = 3600, \
 	template_head = "<ul>\n", \
-	template_list = '<li><span class="#{mm_memo_modified_class(time)}"><a href="#{CGI.escapeHTML(url)}" title="#{CGI.escapeHTML(title)}">#{CGI::escapeHTML(title)}</a></span></li>\n', \
+	template_list = '<li><span class="#{mm_footer_modified_class(time)}"><a href="#{CGI.escapeHTML(url)}" title="#{CGI.escapeHTML(title)}">#{CGI::escapeHTML(title)}</a></span></li>\n', \
 	template_foot = "</ul>\n")
 	url.untaint
 
 	cache_file = "#{@cache_path}/rss-recent/rss-recent.#{CGI.escape(url)}"
 
-	mm_memo_cache_rss(url, cache_file, cache_time.to_i)
+	mm_footer_cache_rss(url, cache_file, cache_time.to_i)
 
 	return '' unless test(?r, cache_file)
 
 	rv = template_head
 
 	i = 0
-	mm_memo_read_from_cache(cache_file).each do |title, url, time, content|
+	mm_footer_read_from_cache(cache_file).each do |title, url, time, content|
 		break if i >= max
 		next if (url.nil? or title.nil?)
 		rv << eval('%Q[' + template_list + ']')
@@ -83,7 +83,7 @@ end
 
 class InvalidResourceError < StandardError; end
 
-def mm_memo_cache_rss(url, cache_file, cache_time)
+def mm_footer_cache_rss(url, cache_file, cache_time)
 
 	cached_time = nil
 	cached_time = File.mtime(cache_file) if File.exist?(cache_file)
@@ -104,7 +104,7 @@ def mm_memo_cache_rss(url, cache_file, cache_time)
 
 			raise URI::InvalidURIError if uri.scheme != "http"
 
-			rss_source = mm_memo_fetch_rss(uri, cached_time)
+			rss_source = mm_footer_fetch_rss(uri, cached_time)
 
 			raise InvalidResourceError if rss_source.nil?
 
@@ -119,25 +119,25 @@ def mm_memo_cache_rss(url, cache_file, cache_time)
 			end
 
 			rss_infos = rss.items.collect do |item|
-				mm_memo_pubDate_to_dc_date(item)
+				mm_footer_pubDate_to_dc_date(item)
 				[item.title, item.link, item.dc_date, item.content_encoded]
 			end
-			mm_memo_write_to_cache(cache_file, rss_infos)
+			mm_footer_write_to_cache(cache_file, rss_infos)
 
 		rescue URI::InvalidURIError
-			mm_memo_write_to_cache(cache_file, [['Invalid URI', url]])
+			mm_footer_write_to_cache(cache_file, [['Invalid URI', url]])
 		rescue InvalidResourceError, ::RSS::Error
-#			mm_memo_write_to_cache(cache_file, [['Invalid Resource', url]])
+#			mm_footer_write_to_cache(cache_file, [['Invalid Resource', url]])
 # when cannot get valid RSS, use old cache
 		end
 	end
 
 end
 
-def mm_memo_fetch_rss(uri, cache_time)
+def mm_footer_fetch_rss(uri, cache_time)
 	rss = nil
 	begin
-		uri.open(mm_memo_http_header(cache_time)) do |f|
+		uri.open(mm_footer_http_header(cache_time)) do |f|
 			case f.status.first
 			when "200"
 				rss = f.read
@@ -156,7 +156,7 @@ def mm_memo_fetch_rss(uri, cache_time)
 	rss
 end
 
-def mm_memo_http_header(cache_time)
+def mm_footer_http_header(cache_time)
 	header = MM_FOOTER_HTTP_HEADER.dup
 	if cache_time.respond_to?(:rfc2822)
 		header["If-Modified-Since"] = cache_time.rfc2822
@@ -164,7 +164,7 @@ def mm_memo_http_header(cache_time)
 	header
 end
 
-def mm_memo_write_to_cache(cache_file, rss_infos)
+def mm_footer_write_to_cache(cache_file, rss_infos)
 	File.open(cache_file, 'w') do |f|
 		f.flock(File::LOCK_EX)
 		rss_infos.each do |info|
@@ -175,7 +175,7 @@ def mm_memo_write_to_cache(cache_file, rss_infos)
 	end
 end
 
-def mm_memo_read_from_cache(cache_file)
+def mm_footer_read_from_cache(cache_file)
 	require 'time'
 	infos = []
 	File.open(cache_file) do |f|
@@ -186,15 +186,15 @@ def mm_memo_read_from_cache(cache_file)
 	end
 	infos.collect do |title, url, time, content|
 		[
-			mm_memo_convert(title),
-			mm_memo_convert(url),
-			mm_memo_convert(time) {|time| Time.parse(time)},
-			mm_memo_convert(content)
+			mm_footer_convert(title),
+			mm_footer_convert(url),
+			mm_footer_convert(time) {|time| Time.parse(time)},
+			mm_footer_convert(content)
 		]
 	end
 end
 
-def mm_memo_convert(str)
+def mm_footer_convert(str)
 	if str.nil? or str.empty?
 		nil
 	else
@@ -206,7 +206,7 @@ def mm_memo_convert(str)
 	end
 end
 
-def mm_memo_pubDate_to_dc_date(target)
+def mm_footer_pubDate_to_dc_date(target)
 	if target.respond_to?(:pubDate)
 		class << target
 			alias_method(:dc_date, :pubDate)
