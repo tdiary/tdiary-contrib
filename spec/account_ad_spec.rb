ba81e9ce83014569cedaa4eb2f894b8148e49b92
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-22 07:42:56 +0000

    platform/tdiary/spec/account_ad_spec.rb: add include_description_about_with.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@5278 d0d07461-0603-4401-acd4-de1884942a52

--- spec/account_ad_spec.rb
+++ spec/account_ad_spec.rb
@@ -1,28 +1,56 @@
 $:.unshift(File.dirname(__FILE__))
 require 'spec_helper'
+require 'time'
 
 describe "account_ad plugin w/" do
-	def setup_account_ad_plugin(service, name)
+	def setup_account_ad_plugin(service, name, mode)
 		fake_plugin(:account_ad) { |plugin|
-			plugin.mode = 'latest'
+			plugin.mode = mode
 			plugin.conf['account.service'] = service
 			plugin.conf['account.name'] = name
 			plugin.conf['base_url'] = 'http://www.hsbt.org/diary/'
+			plugin.date = Time.parse("20070120")
 		}
 	end
 
-	describe "Hatena" do
+	describe "Hatena in day mode" do
 		before do
-			plugin = setup_account_ad_plugin('Hatena', 'hsbt')
+			plugin = setup_account_ad_plugin('Hatena', 'hsbt', 'day')
 			@header_snippet = plugin.header_proc
 		end
+
+		it { @header_snippet.should include_description_about_with(
+				:permalink => 'http://www.hsbt.org/diary/?date=20070120')}
 
 		it { @header_snippet.should include_account_service_with(
 				:service => 'http://www.hatena.ne.jp/')}
 
 		it { @header_snippet.should include_account_name_with(
 				:name => 'hsbt')}
+	end
 
+	describe "Hatena in latest mode" do
+		before do
+			plugin = setup_account_ad_plugin('Hatena', 'hsbt', 'latest')
+			@header_snippet = plugin.header_proc
+		end
+		
+		it { @header_snippet.should include_description_about_with(
+				:permalink => 'http://www.hsbt.org/diary/')}
+
+		it { @header_snippet.should include_account_service_with(
+				:service => 'http://www.hatena.ne.jp/')}
+
+		it { @header_snippet.should include_account_name_with(
+				:name => 'hsbt')}		
+	end
+
+	def include_description_about_with(options)
+		msg = "include #{options[:permalink]}"
+		expected = %|<rdf:Description rdf:about="#{options[:permalink]}">|
+			Spec::Matchers::SimpleMatcher.new(msg) do |actual|
+			actual.include?(expected)
+		end
 	end
 
 	def include_account_service_with(options)
