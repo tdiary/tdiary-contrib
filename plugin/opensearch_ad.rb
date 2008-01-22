47845d7e3347bf07c47b8ad80ebd10f715caf74e
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-22 12:27:28 +0000

    platform/tdiary/plugin/opensearch_ad.rb: refactoring, based openid.rb
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@5292 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/opensearch_ad.rb
+++ plugin/opensearch_ad.rb
@@ -1,34 +1,33 @@
 # opensearch_ad.rb $Revision: 1.1 $
 #
-# Copyright (c) 2006 SHIBATA Hiroshi <h-sbt@nifty.com>
+# Copyright (c) 2008 SHIBATA Hiroshi <h-sbt@nifty.com>
 # Distributed under the GPL
 #
 
-add_header_proc do
-	opensearch_ad_init
-
-	opensearch_xml = @conf['opensearch.xml']
-	opensearch_title = @conf['opensearch.title']
-
-	if opensearch_xml.length > 0 then
-	<<-HTML
-      <link type="application/opensearchdescription+xml" rel="search" title="#{h(opensearch_title)}" href="#{h(opensearch_xml)}">
-	HTML
-	else
-		''
+if /^(latest|day)$/ =~ @mode then
+	if @conf['opensearch.xml'] and @conf['opensearch.title'] then
+		opensearch_xml = @conf['opensearch.xml']
+		opensearch_title = @conf['opensearch.title']
+		
+		add_header_proc do	
+			result = <<-HTML
+			<link type="application/opensearchdescription+xml" rel="search" title="#{h(opensearch_title)}" href="#{h(opensearch_xml)}">
+   	   HTML
+			result.gsub( /^\t\t/, '' )
+		end
 	end
-
 end
 
-def opensearch_ad_init
-	@conf['opensearch.xml'] ||= ""
-	@conf['opensearch.title'] ||= ""
-end
-
-if @mode == 'saveconf'
-	def saveconf_opensearch_ad
+add_conf_proc( 'opensearch_ad', 'OpenSearch Auto-Discovery' ) do
+	if @mode == 'saveconf'
 		@conf['opensearch.xml'] = @cgi.params['opensearch.xml'][0]
 		@conf['opensearch.title'] = @cgi.params['opensearch.title'][0]
 	end
-end
 
+	<<-HTML
+	<h3 class="subtitle">Tilte for OpenSearch</h3>
+	<p><input name="opensearch.title" value="#{h(@conf['opensearch.title'])}" size="80" /></p>
+ 	<h3 class="subtitle">URI for OpenSearch description XML</h3>
+ 	<p><input name="opensearch.xml" value="#{h(@conf['opensearch.xml'])}" size="80" /></p>
+ 	HTML
+end
