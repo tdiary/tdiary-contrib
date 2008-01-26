c5c161697770b0b43201ff1a2b57584ef4e0e33e
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-26 12:59:05 +0000

    platform/tdiary/filter/comment_size.rb: import from my repos.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@5571 d0d07461-0603-4401-acd4-de1884942a52

---
+++ filter/comment_size.rb
@@ -0,0 +1,18 @@
+#
+# comment_size.rb: included TDiary::Filter::CommentSizeFilter class
+#
+
+module TDiary
+   module Filter
+      class CommentSizeFilter < Filter
+         def comment_filter( diary, comment )
+            return false if comment.body.size > @conf['comment.size']
+            true
+         end
+         
+         def referer_filter( referer )
+            true
+         end
+      end
+   end
+end
