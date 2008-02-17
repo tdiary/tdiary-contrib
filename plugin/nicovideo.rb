83e46bbca9d6158bfe1c28c0d5e9ba6f7081c3dc
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2008-02-17 05:19:32 +0000

    platform/tdiary/plugin/nicovideo.rb: added new plugin.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@6804 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/nicovideo.rb
@@ -0,0 +1,76 @@
+#
+# nicovideo.rb - tdiary plugin for Nico Nico Video
+#
+# Copyright (C) TADA Tadashi <sho@spc.gr.jp>
+# Distributed under GPL.
+#
+# usage: <%= nicovideo 'sm99999999' %>
+#
+require 'open-uri'
+require 'timeout'
+require 'rexml/document'
+
+def nicovideo_call_api( video_id )
+	uri = "http://www.nicovideo.jp/api/getthumbinfo/#{video_id}"
+	proxy = @conf['proxy']
+	proxy = 'http://' + proxy if proxy
+	xml = timeout( feed? ? 10 : 2 ) { open( uri, :proxy => proxy ) {|f| f.read } }
+	doc = REXML::Document::new( xml ).root
+	res = doc.elements.to_a( '/nicovideo_thumb_response' )[0]
+	if res.attributes['status'] == 'ok' then
+		res.elements.to_a( 'thumb' )[0]
+	else
+		raise ::Errno::ENOENT::new
+	end
+end
+
+def nicovideo_inline( elem )
+	url = elem.to_a( 'watch_url' )[0].text
+	thumb = elem.to_a( 'thumbnail_url' )[0].text
+	title = elem.to_a( 'title' )[0].text
+	desc = elem.to_a( 'description' )[0].text
+	comment = elem.to_a( 'last_res_body' )[0].text
+	length = elem.to_a( 'length' )[0].text
+	view = elem.to_a( 'view_counter' )[0].text
+	comment_num = elem.to_a( 'comment_num' )[0].text
+	mylist = elem.to_a( 'mylist_counter' )[0].text
+
+	result = <<-TABLE
+		<table border="0" cellpadding="4" cellspacing="0" summary="">
+		<tr valign="top">
+		<td>
+		<div style="margin:4px 0px;"><a href="#{url}" target="_blank"><img alt="#{title}" src="#{thumb}" width="130" height="100" class="thumb_img_normal"></a></div>
+		<p class="TXT10"><strong>#{length}</strong><br>
+		再生: <strong>#{view}</strong><br>
+		コメント: <strong>#{comment_num}</strong><br>
+		マイリスト:<strong>#{mylist}</strong></p>
+		</td>
+		<td width="100%">
+		<p class="TXT12"><a href="#{url}" target="_blank" class="video">#{title}</a><br>#{desc}</p>
+		<div class="thumb_res">
+		<p class="TXT12"><strong>#{comment}</strong></p>
+		</div>
+		</td>
+		</tr>
+		</table>
+	TABLE
+	result.gsub( /^\t*/, '' )
+end
+
+def nicovideo_iframe( video_id )
+	%Q|<iframe src="http://www.nicovideo.jp/thumb/#{video_id}" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="http://www.nicovideo.jp/watch/#{video_id}">#{label || 'link for nicovideo'}</a></iframe>\n|
+end
+
+add_header_proc do
+	'<link rel="stylesheet" type="text/css" href="http://res.nicovideo.jp/css/thumb.css">'
+end
+
+def nicovideo( video_id, label = nil )
+	begin
+		@conf.to_native( nicovideo_inline( nicovideo_call_api( video_id ).elements ), 'UTF-8' )
+	rescue ::Errno::ENOENT
+		"<strong>Sorry, #{video_id} was deleted.</strong>"
+	rescue Timeout::Error
+		nicovideo_iframe( video_id )
+	end
+end
