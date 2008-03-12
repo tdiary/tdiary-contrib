9cbe4b4eaff04df5db0d54b7baeb9bb1bba18195
drry <drry@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-12 21:23:50 +0000

    platform/tdiary/plugin/prettify.rb:  * コメント終端 `-->` を混入してしまったので修正しました。実害はなさそうですけど、あぶない、あぶない。  * インデントを修整しました。
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@7877 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/prettify.rb
+++ plugin/prettify.rb
@@ -6,24 +6,24 @@ if /^(latest|day)$/ =~ @mode then
 		<link href="prettify.css" type="text/css" rel="stylesheet" />
 		<script type="text/javascript" src="prettify.js"></script>
 		<script type="text/javascript"><!--
-		function google_prettify(){
-		var divs=document.getElementsByTagName("div");
-		for(var i=divs.length;i-->0;){
-			if(divs[i].className!="body") continue;
-				var pres=divs[i].getElementsByTagName("pre");
-				for(var j=pres.length;j-->0;){
-					pres[j].className="prettyprint";
+			function google_prettify(){
+				var divs=document.getElementsByTagName("div");
+				for(var i=divs.length;i-- >0;){
+					if(divs[i].className!="body")continue;
+					var pres=divs[i].getElementsByTagName("pre");
+					for(var j=pres.length;j-- >0;){
+						pres[j].className="prettyprint";
+					}
 				}
+				prettyPrint();
 			}
-			prettyPrint();
-		}
-		if(window.addEventListener){
-			window.addEventListener('load',google_prettify,false);
-		}else if(window.attachEvent){
-			window.attachEvent('onload',google_prettify);
-		}else{
-			window.onload=google_prettify;
-		}
+			if(window.addEventListener){
+				window.addEventListener('load',google_prettify,false);
+			}else if(window.attachEvent){
+				window.attachEvent('onload',google_prettify);
+			}else{
+				window.onload=google_prettify;
+			}
 		// --></script>
 		HTML
 	end
