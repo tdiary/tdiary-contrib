2a080143263c094bf4e8159ab2267ce469ccdc7a
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-13 10:45:05 +0000

    platform/tdiary/plugin/microsummary.rb: first import.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@7896 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/en/microsummary.rb
@@ -0,0 +1,18 @@
+# ja/microsummary.rb
+#
+# Japanese resources for microsummary.rb
+#
+# Copyright (c) 2006 elytsllams <smallstyle@gmail.com>
+# Distributed under the GPL
+#
+
+if @mode == 'conf' || @mode == 'saveconf'
+   add_conf_proc( 'microsummary', 'Microsummary Generator' ) do
+      saveconf_microsummary
+      microsummary_init
+      <<-HTML
+      <h3 class="subtitle">URI for microsummary generator XML</h3>
+      <p><input name="generator.xml" value="#{CGI::escapeHTML(@conf['generator.xml'])}" size="60" /></p>
+      HTML
+   end
+end
---
+++ plugin/microsummary.rb
@@ -0,0 +1,23 @@
+# microsummary.rb
+#
+# Copyright (c) 2006 elytsllams <smallstyle@gmail.com>
+# Distributed under the GPL
+#
+
+add_header_proc do
+	generator_xml = @conf['generator.xml']
+	
+	if generator_xml != nil and @mode == 'latest' and !@cgi.valid?( 'date' )
+		%Q|\t<link rel="microsummary" href="#{generator_xml}">\n|
+	end
+end
+
+def microsummary_init
+   @conf['generator.xml'] ||= ""
+end
+
+if @mode == 'saveconf'
+   def saveconf_microsummary
+		@conf['generator.xml'] = @cgi.params['generator.xml'][0]
+	end
+end
