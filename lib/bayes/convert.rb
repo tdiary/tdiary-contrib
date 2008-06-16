28acae576b94901df725ff9b214172eb7e248a06
hiraku <hiraku@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-16 03:44:05 +0000

    Add omitted lib/bayes/convert.rb
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@14114 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/bayes/convert.rb
@@ -0,0 +1,34 @@
+# Copyright (C) 2008, KURODA Hiraku <hiraku@hinet.mydns.jp>
+# You can redistribute it and/or modify it under GPL2. 
+
+require "bayes"
+require "kconv"
+
+module Bayes
+	module CHARSET
+		module EUC
+			KCONV = Kconv::EUC
+		end
+
+		module UTF8
+			KCONV = Kconv::UTF8
+		end
+	end
+
+	class FilterBase
+		def convert_corpus(corpus, to_code, from_code)
+			r = self.class::Corpus.new
+			corpus.each do |k, v|
+				r[k.kconv(to_code::KCONV, from_code::KCONV)] = v
+			end
+			r
+		end
+		private :convert_corpus
+
+		def convert(to_code, from_code)
+			@charset = to_code
+			@ham = convert_corpus(@ham, to_code, from_code)
+			@spam = convert_corpus(@spam, to_code, from_code)
+		end
+	end
+end
