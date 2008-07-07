87c6d57f473e298e9519a80238818fb6cd41fee3
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-07-07 12:42:36 +0000

    initial release.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@15399 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/hide_sidebar_iphone.rb
@@ -0,0 +1,17 @@
+# hide_sidebar_iphone.rb
+#
+# Copyright (C) 2008 SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
+# You can redistribute it and/or modify it under GPL2.
+#
+
+add_header_proc do
+	if @conf.iphone? then
+	<<-CSS
+	<style type="text/css"><!--
+   div.sidebar { 
+      display: none;
+   }
+	--></style>
+	CSS
+	end
+end
