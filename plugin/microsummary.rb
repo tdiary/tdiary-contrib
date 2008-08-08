929ba27882f33a569ab3751f9fc5a1f22521084c
hb <hb@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-08 09:25:30 +0000

    platform/tdiary/plugin/microsummary.rb: added create_xml.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@17267 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/microsummary.rb
+++ plugin/microsummary.rb
@@ -6,18 +6,57 @@
 
 add_header_proc do
 	generator_xml = @conf['generator.xml']
-
+	
 	if generator_xml != nil and @mode == 'latest' and !@cgi.valid?( 'date' )
 		%Q|\t<link rel="microsummary" href="#{generator_xml}" type="application/x.microsummary+xml">\n|
 	end
 end
 
+def create_xml file_name
+	xml = <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<generator xmlns="http://www.mozilla.org/microsummaries/0.1"
+           name="tDiary Entry Title">
+  <template>
+    <transform xmlns="http://www.w3.org/1999/XSL/Transform" version="1.0">
+      <output method="text"/>
+      <template match="/">
+        <value-of select="html/head/title"/>
+        <choose>
+          <when test="boolean(descendant::div[attribute::class='body'][1]/div[attribute::class='section'][last()]/h3)">
+            <text> - </text>
+            <value-of select="descendant::div[attribute::class='body'][1]/div[attribute::class='section'][last()]/h3"/>
+          </when>
+          <when test="boolean(descendant::div[attribute::class='day'][1]/h2)">
+            <text> - </text>
+            <value-of select="descendant::div[attribute::class='day'][1]/h2"/>
+          </when>
+        </choose>
+      </template>
+    </transform>
+  </template>
+  <update interval="240"/>
+  <pages>
+   <include>^#{@conf.base_url.gsub(/\./, '\\.')}$</include>
+  </pages>
+</generator>
+XML
+	
+	begin
+		File::open( file_name, 'w' ) do |f|
+			f.print to_utf8( xml )
+		end
+	rescue
+	end
+end
+
 def microsummary_init
-   @conf['generator.xml'] ||= ""
+	@conf['generator.xml'] ||= ""
+	create_xml( @conf['generator.xml'] ) unless File::exists? @conf['generator.xml']
 end
 
 if @mode == 'saveconf'
-   def saveconf_microsummary
+	def saveconf_microsummary
 		@conf['generator.xml'] = @cgi.params['generator.xml'][0]
 	end
 end
