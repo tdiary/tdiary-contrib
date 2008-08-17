ac610bc2f4d4c4a3ab27c191df5ef1a8d3d1b8b5
mux03 <mux03@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-17 14:12:16 +0000

    added a new plugin for wikiloc.com
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@17796 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/wikiloc.rb
@@ -0,0 +1,27 @@
+#
+# wikiloc.rb: plugin embedding trip map on wikiloc.
+#
+# Copyright (C) 2008 MUNEDA Takahiro <mux03@panda64.net>
+# You can redistribute it and/or modify it under GPL2.
+#
+# derived from everytrail.rb, Copyright (C) 2008 TADA Tadashi <sho@spc.gr.jp>
+#
+# Parameters:
+#  @trip_id  : your trip id
+#  @measures : on/off
+#  @maptype  : M/S/H/T
+#  @size     : [width,height]
+#
+#  Please see the following address about the details:
+#   http://www.wikiloc.com/forum/posts/list/14.page
+#
+# ChangeLog:
+#	20080706: Initial release
+#	20080713: Change default parameters
+#
+def wikiloc( trip_id, measures = "off", maptype = "M", size = [500,400] )
+	size.collect! {|i| i.to_i }
+	size[0] = 500 if size[0] == 0
+	size[1] = 400 if size[1] == 0
+	%Q|<iframe frameBorder="0" src="http://www.wikiloc.com/wikiloc/spatialArtifacts.do?event=view&id=#{h trip_id}&measures=#{h measures}&title=on&near=on&images=on&maptype=#{maptype}" width="#{size[0]}px" height="#{size[1]}px"></iframe>|
+end
