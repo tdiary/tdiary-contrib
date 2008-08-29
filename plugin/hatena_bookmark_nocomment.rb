9cff01e69efc8dd5f78a417ea388f949f44baa98
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-29 23:30:17 +0000

    initial release.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@18450 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/hatena_bookmark_nocomment.rb
@@ -0,0 +1,9 @@
+# hatena_bookmark_nocomment.rb
+#
+# Copyright (c) 2008 SHIBATA  Hiroshi <h-sbt@nifty.com>
+# Distributed under the GPL2
+#
+
+add_header_proc do	
+	%Q|<meta name="Hatena::Bookmark" content="nocomment" />|
+end
