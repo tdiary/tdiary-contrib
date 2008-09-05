4290606ba73765cdfea3eca6dd4cad864b4bf102
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-05 07:43:45 +0000

    fix latest specification for google_analytics.rb
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@18893 d0d07461-0603-4401-acd4-de1884942a52

--- spec/google_analytics_spec.rb
+++ spec/google_analytics_spec.rb
@@ -2,15 +2,16 @@ $:.unshift(File.dirname(__FILE__))
 require 'spec_helper'
 
 describe "google_analytics plugin" do
-	def setup_google_analytics_plugin(profile_id)
+	def setup_google_analytics_plugin(profile_id, mode)
 		fake_plugin(:google_analytics) { |plugin|
+			plugin.mode = mode
 			plugin.conf['google_analytics.profile'] = profile_id
 		}
 	end
 
 	describe "should render javascript" do
 		before do
-			@plugin = setup_google_analytics_plugin('53836-1')
+			@plugin = setup_google_analytics_plugin('53836-1', 'latest')
 		end
 		
 		it "for footer" do
@@ -19,9 +20,20 @@ describe "google_analytics plugin" do
 		end
 	end
 
+	describe "should render javascript" do
+		before do
+			@plugin = setup_google_analytics_plugin('53836-1', 'conf')
+		end
+		
+		it "for footer" do
+			snippet = @plugin.footer_proc
+			snippet.should be_empty
+		end
+	end
+
 	describe "should not render when profile_id is empty" do
 		before do
-			@plugin = setup_google_analytics_plugin(nil)
+			@plugin = setup_google_analytics_plugin(nil, 'latest')
 		end
 		
 		it "for footer" do
@@ -32,12 +44,15 @@ describe "google_analytics plugin" do
 
 	def expected_html_footer_snippet
 		expected = <<-SCRIPT
-		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-		</script>
-		<script type="text/javascript">
-		_uacct = "UA-53836-1";
-		urchinTracker();
-		</script>
+		<script type="text/javascript"><!--
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.") ;
+		document.write(unescape('%3Cscript src="' + gaJsHost + 'google-analytics.com/ga.js" type="text/javascript"%3E%3C/script%3E')) ;
+		// --></script>
+		<script type="text/javascript"><!--
+		var pageTracker = _gat._getTracker("UA-53836-1");
+		pageTracker._initData();
+		pageTracker._trackPageview();
+		// --></script>
 		SCRIPT
 		expected.gsub( /^\t/, '' ).chomp
 	end
