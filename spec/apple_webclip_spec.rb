e52512c9921dd2a5df132be5ccec016be9cd7238
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-14 08:03:59 +0000

    initial release.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@19283 d0d07461-0603-4401-acd4-de1884942a52

---
+++ spec/apple_webclip_spec.rb
@@ -0,0 +1,46 @@
+$:.unshift(File.dirname(__FILE__))
+require 'spec_helper'
+
+describe "apple_webclip plugin" do
+	def setup_apple_webclip_plugin( url )
+		fake_plugin(:apple_webclip) { |plugin|
+			plugin.conf['apple_webclip.url'] = url
+		}
+	end
+
+	describe "url is enabled" do
+		before do
+			plugin = setup_apple_webclip_plugin('http://example.com/example.png')
+			@header_snippet = plugin.header_proc
+		end
+			
+		it "header include url" do 
+			@header_snippet.should == %Q|\t<link rel="apple-touch-icon" href="http://example.com/example.png"/>|
+		end
+	end
+
+	describe "url is disabled" do
+		describe "url is empty" do
+			before do
+				plugin = setup_apple_webclip_plugin('')
+				@header_snippet = plugin.header_proc
+			end
+			
+			it "header is empty" do 
+				@header_snippet.should be_empty
+			end
+		end
+
+		describe "url is nil" do
+			before do
+				plugin = setup_apple_webclip_plugin(nil)
+				@header_snippet = plugin.header_proc
+			end
+			
+			it "header is empty" do 
+				@header_snippet.should be_empty
+			end
+		end
+	end
+
+end
