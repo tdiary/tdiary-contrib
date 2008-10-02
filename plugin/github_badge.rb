f9ce35847e0a3dd0237cb8b226e78082e40dbd77
kakutani <kakutani@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-02 17:18:38 +0000

    documentation and reorganize variables.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@20539 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/github_badge.rb
+++ plugin/github_badge.rb
@@ -2,21 +2,25 @@
 # http://drnicjavascript.rubyforge.org/github_badge/
 #
 # usage:
-#   github_badge(github_username)
-#     - github_username: user name on github.com
+#   github_badge(username, list_length, head, theme, title, show_all)
+#     - username:    user name on github.com
+#     - list_length: project list length
+#     - theme:       specify theme for badge. "white" or "black".
+#     - title:       top text display on the badge.
+#     - show_all:    'Show All' message.
 #
 # Copyright (c) 2008 KAKUTANI Shintaro <http://kakutani.com/>
 # Distributed under the GPL
 
-def github_badge( github_username, list_length = 10, github_head = "div", github_theme = "white", github_title = "My Projects", show_all = "Show all" )
+def github_badge( username, list_length = 10, head = "div", theme = "white", title = "My Projects", show_all = "Show all" )
 	return (<<-EOS).chomp
 <div id="github-badge"></div>
 <script type="text/javascript" charset="utf-8">
-  GITHUB_USERNAME="#{ github_username }";
-  GITHUB_LIST_LENGTH=#{list_length};
-  GITHUB_THEME="#{github_theme}";
-  GITHUB_TITLE="#{github_title}"
-  GITHUB_SHOW_ALL = "Show all"
+  GITHUB_USERNAME="#{ username }";
+  GITHUB_LIST_LENGTH=#{ list_length };
+  GITHUB_THEME="#{ theme }";
+  GITHUB_TITLE="#{ title }"
+  GITHUB_SHOW_ALL = "#{ show_all }"
 </script>
 <script src="http://drnicjavascript.rubyforge.org/github_badge/dist/github-badge-launcher.js" type="text/javascript"></script>
 	EOS
