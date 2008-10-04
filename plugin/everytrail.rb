8ea3bc9bc11dcb4e0f60dc04388fa3f010eee80d
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-04 03:49:43 +0000

    platform/tdiary/plugin/everytrail.rb: fixed sizing bug.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@20669 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/everytrail.rb
+++ plugin/everytrail.rb
@@ -10,5 +10,5 @@ def everytrail( trip_id, label = nil, size = [400,300] )
 	size[0] = 400 if size[0] == 0
 	size[1] = 300 if size[1] == 0
 	l = label ? %Q|<a href="http://www.everytrail.com/view_trip.php?trip_id=#{h trip_id}">#{h label}</a>| : ''
-	%Q|<iframe src="http://www.everytrail.com/iframe2.php?trip_id=#{h trip_id}&width=#{size[0]}height=#{size[1]}" marginheight=0 marginwidth=0 frameborder=0 scrolling=no width=#{size[0]} height=#{size[1]}>#{l}</iframe>|
+	%Q|<iframe src="http://www.everytrail.com/iframe2.php?trip_id=#{h trip_id}&width=#{size[0]}&height=#{size[1]}" marginheight=0 marginwidth=0 frameborder=0 scrolling=no width=#{size[0]} height=#{size[1]}>#{l}</iframe>|
 end
