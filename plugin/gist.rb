f5bfe85c21a5c1775e25127b91afdf7ec7ef0229
poppen <poppen@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-26 09:18:44 +0000

    platform/tdiary/spec/gist_spec.rb: fixed usage.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@29073 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/gist.rb
+++ plugin/gist.rb
@@ -1,7 +1,7 @@
 # show code snippet via gist.github.com
 #
 # usage:
-#   flickr(gist_id)
+#   gist(gist_id)
 #     - gist_id: The id of the code snippet on gitst.github.com (e.g. 2056 for http://gist.github.com/2056)
 #
 # Copyright (c) KAKUTANI Shintaro <http://kakutani.com/>
