89f22a09af566437dabea84b0bb4f1ec3ae75b55
hsbt <hsbt@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-16 13:09:09 +0000

    oops.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@30151 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/canonical.rb
+++ plugin/canonical.rb
@@ -1,7 +1,9 @@
 # canonical.rb
 
-add_header_proc do
-	<<-HTML
-	<link rel="canonical" href="#{@conf.base_url}" />
-	HTML
+if /latest/ =~ @mode then
+	add_header_proc do
+		<<-HTML
+		<link rel="canonical" href="#{@conf.base_url}" />
+		HTML
+	end
 end
