4df4f7fd057fe2e005297f32bbf5e81073e37983
kayakaya <kayakaya@d0d07461-0603-4401-acd4-de1884942a52>
2010-03-31 15:53:52 +0000

    initial commit
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37138 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/latlonglab_route.rb
@@ -0,0 +1,25 @@
+#
+# latlonglab_route.rb - tDiary plugin for LatLongLab Route
+#
+# Copyright (C) 2009, Michitaka Ohno <elpeo@mars.dti.ne.jp>
+# Copyright (C) 2010, KAYA Satoshi <kayakaya@kayakaya.net>
+# You can redistribute it and/or modify it under GPL2.
+#
+
+def route( id, w = 480, h = 480 )
+
+  if feed? or @conf.mobile_agent?
+    return %Q|<p><a href="http://latlonglab.yahoo.co.jp/route/watch?id=#{id}">Link to LatLongLab Route</a></p>|
+  end
+
+  if @conf.iphone?
+    w = 240
+    h = 380
+  end
+
+  <<-HTML
+  <div class="latlonglab-route">
+  <script type="text/javascript" encoding="UTF-8" src="http://latlonglab.yahoo.co.jp/route/paste?id=#{id}&width=#{w}&height=#{h}"></script>
+  </div>
+  HTML
+end
