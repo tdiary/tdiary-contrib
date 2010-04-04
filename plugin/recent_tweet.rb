d5bdc876b2a1bd4110ee4bc631852915268f2da5
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2010-04-04 22:25:49 +0000

    renamed twitter_badge to recent_tweet.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37159 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/recent_tweet.rb
+++ plugin/recent_tweet.rb
@@ -1,5 +1,5 @@
 #
-# twitter_badge.rb: twitter status plugin for tDiary
+# recemt_tweet.rb: twitter status plugin for tDiary
 #
 # Copyright (C) 2007 by Nishimoto Masaki <gaju@gaju.jp>
 # Distributed under GPL.
@@ -9,43 +9,43 @@ require 'open-uri'
 require 'timeout'
 require 'rexml/document'
 
-def twitter_badge( id, count )
+def recent_tweet( id, count )
 	begin
-		cache = "#{@cache_path}/twitter_badge.xml"
+		cache = "#{@cache_path}/recent_tweet.xml"
 		xml = open( cache ) {|f| f.read }
 		if Time::now > File::mtime( cache ) + 10*60 then
 			File::delete( cache )  # clear cache 10 minutes later
 		end
 	rescue Errno::ENOENT
 		begin
-			xml = twitter_badge_call_api( id )
+			xml = recent_tweet_call_api( id )
 			open( cache, 'wb' ) {|f| f.write( xml ) }
 		rescue Timeout::Error, StandardError
-			return %Q|<div class="twitter-badge error">twitter_badge: #{$!}</div>|
+			return %Q|<div class="recent_tweet error">recent_tweet: #{$!}</div>|
 		end
 	end
 
 	begin
 		doc = REXML::Document::new( xml )
 		if doc then
-			html = '<div class="twitter-badge">'
-			html << '<p class="twitter-badge-title">'
+			html = '<div class="recent-tweet">'
+			html << '<p class="recent-tweet-title">'
 			html << %Q|<a href="http://twitter.com/#{id}">| << 'What am I doing...</a>'
 			html << '</p>'
-			html << '<ul class="twitter-badge-body">'
+			html << '<ul class="recent-tweet-body">'
 			i = 0
 			doc.elements.each( 'statuses/status' ) do |status|
 				created_at = Time.parse( status.elements.to_a( 'created_at' )[0].text )
 				text = status.elements.to_a( 'text' )[0].text
 				if /^\@(.*)?/.match( text ) == nil and i < count then
-					html << '<li class="twitter-badge-status">'
+					html << '<li class="recent-tweet-status">'
 					if Time.now > created_at + 60*60*23 then
 						time = created_at.localtime.strftime( '%b %d %H:%M' )
 					else
 						time = created_at.localtime.strftime( '%H:%M' )
 					end
-					html << '<span class="twitter-badge-text">' << %Q|#{text}| << '</span> '
-					html << '<span class="twitter-badge-time">(' << %Q|#{time}| << ')</span>'
+					html << '<span class="recent-tweet-text">' << %Q|#{text}| << '</span> '
+					html << '<span class="recent-tweet-time">(' << %Q|#{time}| << ')</span>'
 					html << '</li>'
 					i += 1
 				end
@@ -53,14 +53,14 @@ def twitter_badge( id, count )
 			html << '</ul></div>'
 			@conf.to_native( html )
 		else
-			return '<div class="twitter-badge error">twitter_badge: Failed to open file</div>'
+			return '<div class="recent-tweet error">recent_tweet: Failed to open file</div>'
 		end
 	rescue REXML::ParseException
-		return '<div class="twitter-badge error">twitter_badge: Failed to parse XML</div>'
+		return '<div class="recent-tweet error">recent_tweet: Failed to parse XML</div>'
 	end
 end
 
-def twitter_badge_call_api( id )
+def recent_tweet_call_api( id )
 	request = "http://twitter.com/statuses/user_timeline/#{id}.xml"
 
 	proxy = @conf['proxy']
