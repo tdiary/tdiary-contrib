3545c9e0df7965104269541154da61c1aeebf741
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2010-04-05 08:52:27 +0000

    added twitter_badge plugin.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37160 d0d07461-0603-4401-acd4-de1884942a52

---
+++ plugin/twitter_badge.rb
@@ -0,0 +1,42 @@
+#
+# twitter_badge.rb: insert 'Follow me' badge of Twitter.
+#
+# Copyright (C) 2010 TADA Tadashi <t@tdtds.jp>
+# You can redistribute it and/or modify it under GPL2.
+#
+
+def twitter_badge( account, opt = {} )
+	return '' unless account
+	@twitter_badge_setting = {
+		:account => account,
+		:label => (opt[:label] || 'follow-me'),
+		:color => (opt[:color] || '#35ccff'),
+		:side => (opt[:side] || 'right')[0,1],
+		:top => (opt[:top] || 136).to_i,
+		:delay => (opt[:delay] || 5).to_i * 1000,
+	}
+	@twitter_badge_setting.inspect # do nothing in this method.
+end
+
+add_footer_proc do
+	if @twitter_badge_setting then
+		t = @twitter_badge_setting
+		<<-TEXT
+		<!-- twitter follow badge by go2web20 -->
+		<script src="http://files.go2web20.net/twitterbadge/1.0/badge.js" type="text/javascript"></script>
+		<script type='text/javascript' charset='utf-8'><!--
+		tfb.account = '#{t[:account]}';
+		tfb.label = '#{t[:label]}';
+		tfb.color = '#{t[:color]}';
+		tfb.side = '#{t[:side]}';
+		tfb.top = #{t[:top]};
+		setTimeout( tfb.showbadge, #{t[:delay]} );
+		//-->
+		</script>
+		<!-- end of twitter follow badge -->
+		TEXT
+	else
+		''
+	end
+end
+
