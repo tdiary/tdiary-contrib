6add5a7cd3359548bb4c7542e5244eb0a2f3c13e
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2010-04-09 13:55:46 +0000

    specified class name and size parameter.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37176 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/youtube.rb
+++ plugin/youtube.rb
@@ -5,14 +5,14 @@
 #
 # usage: <%= youtube 'VIDEO_ID' %>
 #
-def youtube( video_id )
+def youtube( video_id, size = [425,350] )
 	if @conf.mobile_agent? or @conf.iphone? or feed? then
 		%Q|<div class="youtube"><a href="http://www.youtube.com/watch?v=#{video_id}">YouTube (#{video_id})</a></div>|
 	elsif defined?( :iphone? ) and iphone?
 		%Q|<div class="youtube"><a href="youtube:#{video_id}">YouTube (#{video_id})</a></div>|
 	else
 		<<-TAG
-		<object width="425" height="350"><param name="movie" value="http://www.youtube.com/v/#{video_id}"></param><embed src="http://www.youtube.com/v/#{video_id}" type="application/x-shockwave-flash" width="425" height="350"></embed></object>
+		<object class="youtube" width="#{size[0]}" height="#{size[1]}"><param name="movie" value="http://www.youtube.com/v/#{video_id}"></param><embed src="http://www.youtube.com/v/#{video_id}" type="application/x-shockwave-flash" width="#{size[0]}" height="#{size[1]}"></embed></object>
 		TAG
 	end
 end
