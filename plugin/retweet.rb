75c657f0e9b84ec225e8a7f604a595ae8ac825bb
drry <drry@d0d07461-0603-4401-acd4-de1884942a52>
2010-04-26 08:15:59 +0000

    * fixed regexes.  * et cetera.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37280 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/retweet.rb
+++ plugin/retweet.rb
@@ -1,44 +1,46 @@
 #
 # retweet.rb - show retweet count in each section
-#   This plugin uses a retweet button script powered by topsy.com
+#   This plugin uses a Topsy Retweet Button for Web Sites powered by Topsy.com
 #
 # Copyright (C) 2010, MATSUOKA Kohei <http://www.machu.jp/diary/>
 # You can redistribute it and/or modify it under GPL2.
 #
 
 #
-# topsy settings. see: http://labs.topsy.com/button/retweet-button/#global_settings
+# settings for Topsy Retweet Button.
+# see: http://labs.topsy.com/button/retweet-button/#global_settings
 #
-# your twitter nickname
+# your Twitter nickname
 @topsy_nick = "your_twitter_account"
 # retweet button color
-@topsy_theme = "blue"
+#@topsy_theme = "blue"
 # retweet button text
-@topsy_retweet_text = "ReTweet"
+#@topsy_retweet_text = "retweet"
 
 unless defined?(permalink)
   def permalink( date, index, escape = true )
     ymd = date.strftime( "%Y%m%d" )
     uri = @conf.index.dup
-    uri[0, 0] = @conf.base_url unless %r|^https?://|i =~ uri
-    uri.gsub!( %r|/\./|, '/' )
+    uri.sub!( %r|\A(?!https?://)|i, @conf.base_url )
+    uri.tr!( ".", "/" )
     link = uri + anchor( "#{ymd}p%02d" % index )
-    link = link.sub( /#/, "%23" ) if escape
+    link.sub!( "#", "%23" ) if escape
     link
   end
 end
 
 unless defined?(subtitle)
   def subtitle( date, index, escape = true )
+    diary = @diaries[date.strftime( "%Y%m%d" )]
+    return "" unless diary
     sn = 1
-    diary = @diaries[date.strftime( '%Y%m%d' )]
-    diary && diary.each_section do |section|
-      if sn == index then
-        old_apply_plugin = @options['apply_plugin']
-        @options['apply_plugin'] = true
+    diary.each_section do |section|
+      if sn == index
+        old_apply_plugin = @options["apply_plugin"]
+        @options["apply_plugin"] = true
         title = apply_plugin( section.subtitle_to_html, true )
-        @options['apply_plugin'] = old_apply_plugin
-        title.gsub!( /"/, '\"' ) if escape
+        @options["apply_plugin"] = old_apply_plugin
+        title.gsub!( /(?=")/, "\\" ) if escape
         return title
       end
       sn += 1
@@ -46,21 +48,22 @@ unless defined?(subtitle)
   end
 end
 
-# load tospy script and initialize
+# load Tospy's script and initialize
 add_header_proc do
-	r = ''
-  r << %Q|<script type="text/javascript" src="http://cdn.topsy.com/topsy.js?init=topsyWidgetCreator"></script>|
-  r << %Q|<script type="text/javascript" id="topsy_global_settings">|
-  r << %Q|  var topsy_theme = "#{@topsy_theme}";| if @topsy_theme
-  r << %Q|  var topsy_nick = "#{@topsy_nick}";| if @topsy_nick
-  r << %Q|  var topsy_retweet_text = "#{@topsy_retweet_text}"| if @topsy_retweet_text
-  r << %Q|</script>|
+  r = ""
+  r << %Q|<script type="text/javascript" src="http://cdn.topsy.com/topsy.js?init=topsyWidgetCreator"></script>\n|
+  return r unless @topsy_theme or @topsy_nick or @topsy_retweet_text
+  r << %Q|<script type="text/javascript" id="topsy_global_settings"><!--\n|
+  r << %Q|  var topsy_theme = "#{@topsy_theme}";\n| if @topsy_theme
+  r << %Q|  var topsy_nick = "#{@topsy_nick}";\n| if @topsy_nick
+  r << %Q|  var topsy_retweet_text = "#{@topsy_retweet_text}";\n| if @topsy_retweet_text
+  r << %Q|//--></script>\n|
 end
 
 # show retweet button in top of section
 add_section_enter_proc do |date, index|
   <<-"EOS"
-  <div class="topsy_widget_data" style="float: right; margin-left: 1em;"> <!-- {
+  <div class="topsy_widget_data" style="float: right; margin-left: 1em;"><!-- {
     "url": "#{permalink(date, index)}",
     "title": "#{subtitle(date, index)}",
     "style": "big"
@@ -72,8 +75,8 @@ end
 add_section_leave_proc do |date, index|
   <<-"EOS"
   <div class="tags">
-	  <img src="http://www.machu.jp/diary/twitter_logo_small.png" height="16" width="62" alt="Twitter">: 
-    <div class="topsy_widget_data" style="float: right; margin-left: 1em;"> <!-- {
+    <img src="http://www.machu.jp/diary/twitter_logo_small.png" height="16" width="62" alt="Twitter">:
+    <div class="topsy_widget_data" style="float: right; margin-left: 1em;"><!-- {
       "url": "#{permalink(date, index)}",
       "title": "#{subtitle(date, index)}",
       "style": "small"
