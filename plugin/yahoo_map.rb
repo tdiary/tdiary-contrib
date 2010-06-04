06383cb09aedf736208281d3d8cb24fe3acc8fce
drry <drry@d0d07461-0603-4401-acd4-de1884942a52>
2010-06-04 15:36:22 +0000

    * removed a regex.  * et cetera.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@37797 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/yahoo_map.rb
+++ plugin/yahoo_map.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# yahoo_map.rb - embeded Yahoo! Japan Map for tDiary
+# yahoo_map.rb - embeded Yahoo! JAPAN Map for tDiary
 #
 # Copyright (C) 2010, KAYA Satoshi <kayakaya@kayakaya.net>
 # You can redistribute it and/or modify it under GPL2.
@@ -8,23 +8,23 @@
 
 add_header_proc do
    init_ymap
+   r = ''
    if @conf['yahoo_jp.appid'] and @conf['yahoo_jp.appid'].size > 0
-      %Q|<script type="text/javascript"
-       src="http://map.yahooapis.jp/MapsService/js/V2/?appid=#{h @conf['yahoo_jp.appid']}"></script>|
-   else
-      ''
+      r << %Q|<script type="text/javascript"|
+      r << %Q| src="http://map.yahooapis.jp/MapsService/js/V2/?appid=#{h @conf['yahoo_jp.appid']}"></script>|
    end
+   return r
 end
 
-add_conf_proc( 'yahoo_jp_appid', 'Yahoo! Japan Application ID' ) do
+add_conf_proc( 'yahoo_jp_appid', 'Yahoo! JAPAN Application ID' ) do
    if @mode == 'saveconf' then
       @conf['yahoo_jp.appid'] = @cgi.params['yahoo_jp.appid'][0]
    end
 
    <<-HTML
-   <h3 class="subtitle">Yahoo! Japan Application ID</h3>
+   <h3 class="subtitle">Yahoo! JAPAN Application ID</h3>
    <p><input name="yahoo_jp.appid" value="#{h @conf['yahoo_jp.appid']}" size="70"></p>
-        <p><a href="http://e.developer.yahoo.co.jp/webservices/register_application">Get Application id</a></p>
+   <p><a href="http://e.developer.yahoo.co.jp/webservices/register_application">Get Application id</a></p>
    HTML
 end
 
@@ -37,8 +37,8 @@ def init_ymap
 end
 
 def generate_ymapid(lat, lon, layer)
-   ymapid = "ymapid" + lat.to_s + lon.to_s + layer.to_s
-   ymapid.gsub!(/\./,'')
+   ymapid = 'ymapid' << lat.to_s << lon.to_s << layer.to_s
+   ymapid.delete('.')
 end
 
 def yahoo_map(lat, lon, options = {})
@@ -46,7 +46,7 @@ def yahoo_map(lat, lon, options = {})
    options[:size] ||= 'medium'
 
    if feed? or @conf.mobile_agent? then
-      return %Q|<p><a href="http://map.yahoo.co.jp/pl?type=scroll&lat=#{lat}&lon=#{lon}&z=17&mode=map&pointer=on&datum=wgs&fa=ks&home=on&hlat=#{lat}&hlon=#{lon}&layout=&ei=utf-8&p=">Link to Yahoo! Japan Map </a></p>|
+      return %Q|<p><a href="http://map.yahoo.co.jp/pl?type=scroll&amp;lat=#{lat}&amp;lon=#{lon}&amp;z=17&amp;mode=map&amp;pointer=on&amp;datum=wgs&amp;fa=ks&amp;home=on&amp;hlat=#{lat}&amp;hlon=#{lon}&amp;layout=&amp;ei=utf-8&amp;p=">Link to Yahoo! JAPAN Map </a></p>|
    end
 
    # define map size
@@ -63,28 +63,27 @@ def yahoo_map(lat, lon, options = {})
 
    @ymap_container << ymap_info
 
-   r = %Q|<div class="ymap" id="#{ymapid}" style="width:#{width[size]}; height:#{height[size]}"></div>|
-   return r
+   %Q|<div class="ymap" id="#{ymapid}" style="width:#{width[size]}; height:#{height[size]}"></div>|
 end
 
 def insert_ymap_js
+   r = ''
    if @ymap_container.size > 0 then
       unless feed? then
-         r = ""
          r << %Q|<script type="text/javascript">\n|
          r << %Q|function defineYmapIds() {\n|
          @ymap_container.each do |ymap_info|
-            r << %Q|var obj#{ymap_info[:ymapid]} = new YahooMapsCtrl("#{ymap_info[:ymapid]}", "#{ymap_info[:lat]}, #{ymap_info[:lon]}", #{ymap_info[:layer]}, YMapMode.MAP, YDatumType.WGS84);\n|
+            r << %Q|  var obj#{ymap_info[:ymapid]} = new YahooMapsCtrl("#{ymap_info[:ymapid]}", "#{ymap_info[:lat]}, #{ymap_info[:lon]}", #{ymap_info[:layer]}, YMapMode.MAP, YDatumType.WGS84);\n|
          end
          r << %Q|}\n|
-         r << %Q|if (window.addEventListener) window.addEventListener('load', defineYmapIds , false); // for Firefox\n|
-         r << %Q|if (window.attachEvent) window.attachEvent('onload', defineYmapIds); //for IE\n|
+         r << %Q|if (window.addEventListener) window.addEventListener("load", defineYmapIds, false); // for DOM level 2 compliant Web browsers\n|
+         r << %Q|else if (window.attachEvent) window.attachEvent("onload", defineYmapIds); // for IE\n|
          r << %Q|</script>|
-         return r
+      #else
+      #   return
       end
-   else
-      ''
    end
+   return r
 end
 # Local Variables:
 # mode: ruby
