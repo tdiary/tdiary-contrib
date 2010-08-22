de81c8504691afe61997fa16b72e8fff98cc8611
machu <machu@d0d07461-0603-4401-acd4-de1884942a52>
2010-08-22 05:57:47 +0000

    add Array.prototype map function for IE8
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@38325 d0d07461-0603-4401-acd4-de1884942a52

--- js/draft.js
+++ js/draft.js
@@ -8,6 +8,25 @@ $(function() {
 
 if (!localStorage) { return; }
 
+// for compatibility (ex: IE8)
+// from https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Objects/Array/Map
+if (!Array.prototype.map) {
+  Array.prototype.map = function(fun /*, thisp*/) {
+    var len = this.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++) {
+      if (i in this)
+      res[i] = fun.call(thisp, this[i], i, this);
+    }
+
+    return res;
+  };
+}
+
 var Draft = function(storage, text) {
   // 保存先のストレージ
   this.storage = storage;
