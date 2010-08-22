b5d602634f1562e3fc62c3b8bef5d8d0f3fa3a08
sho <sho@d0d07461-0603-4401-acd4-de1884942a52>
2010-08-22 13:22:18 +0000

    updated rubykaigi badges for rubykaigi2010.
    git-svn-id: http://svn.coderepos.org/share/platform/tdiary@38333 d0d07461-0603-4401-acd4-de1884942a52

--- plugin/rubykaigi.rb
+++ plugin/rubykaigi.rb
@@ -1,12 +1,32 @@
 #
-# rubykaigi2008.rb: make badge of RubyKaigi2008.
+# rubykaigi.rb: make badges of RubyKaigi.
 #
-# usage: <%= rubykaigi2008 'role' %>
-#    role: attendee (default), speaker, sponsor, staff
+# usage: <%= rubykaigi 'role' %>
+#    role: attendee (default), speaker, sponsor, staff, committer, individual sponsor
 #
-# Copyright (C) TADA Tadashi <sho@spc.gr.jp>
+# Copyright (C) TADA Tadashi <t@tdtds.jp>
 # Distributed under GPL.
 #
+
+def rubykaigi2010( role = 'attendee' )
+	badges = Hash::new( 'attendee' ).update({
+		'committer' => 'committer',
+		'individual sponsor' => 'individual_sponsor',
+		'sponsor' => 'sponsor',
+		'staff' => 'staff',
+		'speaker' => 'speaker',
+		'attendee' => 'attendee'
+	})
+	%Q|<a href="http://rubykaigi.org/2010/" style="display:block;margin:8px auto;text-align:center;"><img src="http://rubykaigi.org/2010/badge/#{badges[role]}.png" width="160" height="201" alt="RubyKaigi2010 #{h badges[role].capitalize}" style="border-width: 0px;"></a>|
+end
+
+alias :rubykaigi :rubykaigi2010
+
+#----- OLD EDITIONS -----#
+
+def rubykaigi2009( role = 'attendee' )
+	%Q|<div style="text-align: center; margin-top: 0.5em; margin-bottom: 0.5em;"><a href="http://rubykaigi.org/2009/"><img src="http://rubykaigi.org/2009/images/goodies/badges/#{role}.gif" width="160" height="160" alt="RubyKaigi2009#{h role.capitalize}" style="border-width: 0px;"></a></div>|
+end
 
 def rubykaigi2008( role = 'attendee' )
 	img = case role
@@ -21,11 +41,6 @@ def rubykaigi2008( role = 'attendee' )
 		0
 	end
 	%Q|<div style="text-align: center; margin-top: 0.5em; margin-bottom: 0.5em;"><a href="http://jp.rubyist.net/RubyKaigi2008/"><img src="http://rubykaigi.tdiary.net/images/20080617_#{img}.png" width="160" height="79" alt="RubyKaigi2008#{h role.capitalize}" style="border-width: 0px;"></a></div>|
-end
-
-
-def rubykaigi2009( role = 'attendee' )
-	%Q|<div style="text-align: center; margin-top: 0.5em; margin-bottom: 0.5em;"><a href="http://rubykaigi.org/2009/"><img src="http://rubykaigi.org/2009/images/goodies/badges/#{role}.gif" width="160" height="160" alt="RubyKaigi2009#{h role.capitalize}" style="border-width: 0px;"></a></div>|
 end
 
 def sappororubykaigi02( role = 'attendee' )
