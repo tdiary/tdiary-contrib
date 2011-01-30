c77de8b628fb6dac517dfcef8c493fccc374c246
KADO Masanori <kdmsnr@gmail.com>
2011-01-31 03:06:47 +0900

    add section_link

---
+++ util/section_link/misc/plugin/section_link.rb
@@ -0,0 +1,43 @@
+# Copyright (C) 2011, KADO Masanori <kdmsnr@gmail.com>
+# You can redistribute it and/or modify it under GPL.
+#
+# section_link.rb
+# - enables section link and shows section title
+# - depends on:
+#   - tdiary/tdiary_day_ext.rb
+#   - tdiary/tdiaryext_style.rb
+#   - tdiary/wikiext_style.rb
+
+alias :_orig_subtitle_link :subtitle_link
+def subtitle_link( date, index, subtitle )
+	if @cgi.params['section'].join('') != ''
+		index = @cgi.params['section'][0]
+	end
+	r = _orig_subtitle_link( date, index, subtitle )
+	r.gsub(/#p(\d+)/){"&section=#{$1}"}
+end
+
+alias :_orig_title_tag :title_tag
+def title_tag
+	if @cgi.params['section'].join('') != '' and
+			@mode == 'day' and diary = @diaries[@date.strftime('%Y%m%d')]
+		sec = []
+		diary.each_section do |s|
+			sec << s
+		end
+		site_title = " - #{_orig_title_tag.gsub( /<.*?>/, '')}"
+		title = sec[@cgi.params['section'][0].to_i - 1].stripped_subtitle_to_html
+		return "<title>#{apply_plugin(title, true).gsub(/[\r\n]/, '')}#{h site_title }</title>"
+
+	else
+		_orig_title_tag
+	end
+end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vim: ts=3
---
+++ util/section_link/tdiary/tdiaryext_style.rb
@@ -0,0 +1,36 @@
+# Copyright (C) 2011, KADO Masanori <kdmsnr@gmail.com>
+# You can redistribute it and/or modify it under GPL.
+require 'tdiary/tdiary_day_ext'
+
+module TDiary
+	class TdiaryextDiary; end
+
+	class TdiaryDiary
+		attr_accessor :section_no
+		alias :_orig_to_html4 :to_html4
+		def to_html4( opt )
+			if @section_no
+				idx = @section_no.to_i
+				@sections = [@sections[idx - 1]]
+			end
+			_orig_to_html4 opt
+		end
+
+		alias :_orig_to_chtml :to_chtml
+		def to_chtml( opt )
+			if @section_no
+				idx = @section_no.to_i
+				@sections = [@sections[idx - 1]]
+			end
+			_orig_to_chtml opt
+		end
+	end
+end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vim: ts=3
