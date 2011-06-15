028cc1ddaaadcfb96620874bc76937e8538ad6b6
MATSUOKA Kohei <kohei@machu.jp>
2011-06-15 23:21:59 +0900

    * plugin/draft.rb: use enable_js to load JavaScript

--- plugin/draft.rb
+++ plugin/draft.rb
@@ -5,12 +5,8 @@
 # Copyright (c) MATSUOKA Kohei <http://www.machu.jp/>
 # Distributed under the GPL
 #
-add_header_proc do
-	if /\A(form|edit|preview|showcomment)\z/ === @mode then
-		%Q[<script src="js/draft.js" type="text/javascript"></script>]
-	else
-		''
-	end
+if /\A(form|edit|preview|showcomment)\z/ === @mode then
+	enable_js('draft.js')
 end
 
 add_edit_proc do
