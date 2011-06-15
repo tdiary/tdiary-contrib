462806f90ade2fc0a55e66c8280d1b77e2c8f943
MATSUOKA Kohei <kohei@machu.jp>
2011-06-15 19:19:26 +0900

    * plugin/development.rb: added dev utilities plugin

---
+++ plugin/development.rb
@@ -0,0 +1,12 @@
+# development.rb - utility methods for plugin developers
+#
+# Copyright (c) 2011 MATSUOKA Kohei <kmachu@gmail.com>
+# You can redistribute it and/or modify it under GPL2.
+#
+
+# load standard version jquery.js instead of jquery.min.js
+alias :jquery_tag_original :jquery_tag
+def jquery_tag
+	jquery_tag_original.gsub(/\.min/, '')
+end
+
