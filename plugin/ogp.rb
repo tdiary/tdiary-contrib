8c962a910c9f0399cabf526da3bea6a123c7a7df
MATSUOKA Kohei <kohei@machu.jp>
2011-07-16 18:32:43 +0900

    * plugin/ogp.rb: show og:description w/ section_permalink.rb

--- plugin/ogp.rb
+++ plugin/ogp.rb
@@ -7,18 +7,31 @@
 # @conf['ogp.facebook.app_id'] - your facebook application ID.
 # @conf['ogp.facebook.admins'] - your facebook ID.
 
+def ogp_description
+	section_index = @cgi.params['p'][0]
+	# section_index = "1"
+	if @mode == 'day' and section_index
+		diary = @diaries[@date.strftime('%Y%m%d')]
+		sections = diary.instance_variable_get(:@sections)
+		section = sections[section_index.to_i - 1].body_to_html
+		@conf.shorten(apply_plugin(section, true), 200)
+	else
+		@conf.description
+	end
+end
+
 add_header_proc do
 	headers = {
+		# TODO: og:urlへ対応
 		'og:title' => title_tag.match(/>([^<]+)</).to_a[1],
 		'og:site_name' => @conf.html_title,
-		'og:description' => @conf.description,
+		'og:description' => ogp_description,
 		'og:image' => @conf.banner,
 		'og:type' => (@mode == 'day') ? 'article' : 'blog',
 		'og:author' => @conf.author_name,
 		'fb:app_id' => @conf['ogp.facebook.app_id'],
 		'fb:admins' => @conf['ogp.facebook.admins']
 	}
-	# headers['og:type'] = (@mode == 'day') ? 'article' : 'blog'
 	headers.select {|key, val|
 		val && !val.empty?
 	}.map {|key, val|
