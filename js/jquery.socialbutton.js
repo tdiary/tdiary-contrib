fc73fd5fb783dc250b1689aba36e19721cd745c7
TADA Tadashi <t@tdtds.jp>
2012-03-07 15:16:00 +0900

    replaced script of hatena bookmark to no tracking version.

--- js/jquery.socialbutton.js
+++ js/jquery.socialbutton.js
@@ -655,7 +655,7 @@ function socialbutton_hatena(target, options, defaults, index, max_index)
 	});
 	
 	var tag = '<a' + attr + '><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>'
-			+ '<script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
+			+ '<script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button_wo_al.js" charset="utf-8" async="async"></script>';
 
 	$(target).html(tag);
 }
