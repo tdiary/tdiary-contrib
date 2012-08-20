d68f09f17bfd6863a3114810f6b6a04b22fa83a2
MATSUOKA Kohei <kohei@machu.jp>
2012-08-21 01:11:49 +0900

    notify only unchecked update

--- js/plugin_checker.js
+++ js/plugin_checker.js
@@ -8,6 +8,20 @@
 $(function() {
   var url = 'http://tdiary-plugin-checker.herokuapp.com/t_diary_plugin_files.json';
 
+  function saveCheckedDate(date) {
+    if (!localStorage) { return; }
+    var pluginChecker = localStorage.pluginChecker ? JSON.parse(localStorage.pluginChecker) : {};
+    pluginChecker.checkedDate = date;
+    localStorage.pluginChecker = JSON.stringify(pluginChecker);
+  }
+
+  function loadCheckedDate() {
+    if (!localStorage || !localStorage.pluginChecker) {
+      return null;
+    }
+    return (JSON.parse(localStorage.pluginChecker)).checkedDate;
+  }
+
   function createNotify(plugins) {
     var ul = $('<ul>');
     $.each(plugins, function(i, plugin) {
@@ -25,7 +39,10 @@ $(function() {
     var close = $('<div style="float: right">')
       .append(
         $('<button style="padding: 0.5em">閉じる</button>')
-          .click(function() { notify.hide(); })
+          .click(function() {
+            saveCheckedDate(plugins[0].commit.date);
+            notify.hide();
+          })
       );
     var notify = $('<div class="plugin_checker">プラグインの更新が見つかりました。</div>')
       .append(close)
@@ -36,11 +53,15 @@ $(function() {
 
   $.get(url, function(plugins) {
     var now = new Date();
+    var checkedDate = loadCheckedDate();
     var updates = $.grep(plugins, function(plugin) {
-      var age = parseInt((now - new Date(plugin.commit.date)) / 1000 / 3600 / 24);
-      // TODO: 条件を追加する（使用中プラグインかつ未チェック）
-      // 最終チェック日をlocalStorageに保存しておく
-      return (age < 7);
+      // TODO: 条件を追加する（使用中プラグイン）
+      if (checkedDate) {
+        return (checkedDate < plugin.commit.date);
+      } else {
+        var age = parseInt((now - new Date(plugin.commit.date)) / 1000 / 3600 / 24);
+        return (age < 7);
+      }
     });
     if (updates.length > 0) {
       $('form.update').before(createNotify(updates));
