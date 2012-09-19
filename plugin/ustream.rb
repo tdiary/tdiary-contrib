708a4762aeb4382a8e0d4e10d8843097d541a5f7
Kouhei Sutou <kou@clear-code.com>
2012-09-19 15:11:44 +0900

    Use iframe for embedding Ustream player
    Because the official site generates iframe tag instead of object tag
    for embedding.
    
    Try clicking "share" button in player.

--- plugin/ustream.rb
+++ plugin/ustream.rb
@@ -15,8 +15,5 @@ def ustream( id, type = :recorded )
 		return %Q|<a href="http://www.ustream.tv/recorded/#{id}">Link to Ustream ##{id}</a></p><p>|
 	end
 
-	# flashvars+="locale=(ja_JP|en_US)"
-	utv_id = "utv#{rand(1000000)}"
-	utv_name = "utv_n_#{rand(1000000)}"
-	%Q|<object class="ustream" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="480" height="386" id="#{utv_id}" name="#{utv_name}"><param name="flashvars" value="autoplay=false"><param name="allowfullscreen" value="true"><param name="allowscriptaccess" value="always"><param name="src" value="http://www.ustream.tv/flash/video/#{id}"><embed flashvars="autoplay=false" width="480" height="386" allowfullscreen="true" allowscriptaccess="always" id="#{utv_id}" name="#{utv_name}" src="http://www.ustream.tv/flash/video/#{id}" type="application/x-shockwave-flash"></embed></object>|
+	%Q|<iframe class="ustream" width="480" height="302" src="http://www.ustream.tv/embed/recorded/#{id}?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>|
 end
