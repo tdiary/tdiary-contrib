0ef2e6756cc4102944fc6dfe728922842fc16b3c
MATSUOKA Kohei <kohei@machu.jp>
2013-01-19 02:24:46 +0900

    support for disp_referer.rb

--- js/referer.js
+++ js/referer.js
@@ -6,45 +6,19 @@
  */
 
 $(function() {
-  var referer_div = $('div#referer');
-  var endpoint = referer_div.data('endpoint');
-  var date = referer_div.data('date');
-  var button = $('<button></button>').text($tDiary.plugin.referer.today);
-
-  function create_referer_list (items) {
-    var referer_list = $('<ul>');
-    $.each(items, function(i, item) {
-      referer_list.append(
-        $('<li>')
-          .append($('<span>').text(item.count + ' '))
-          .append($('<a>').attr('href', item.href).text(item.title))
-      );
-    });
-    return referer_list;
-  }
+  var button = $('.lazy_referer');
+  var endpoint = button.data('endpoint');
 
   button.click(function() {
     button.attr("disabled", "disabled");
     $.ajax({
       type: 'GET',
-      dataType: "json",
-      url: referer_div.data('endpoint'),
+      dataType: "html",
+      url: endpoint,
       success: function(data) {
-        // console.debug(data);
-        if (data['volatile']) {
-          var volatile_referer_div = $('<div>')
-            .attr('class', 'caption')
-            .text($tDiary.plugin.referer.volatile)
-            .after(create_referer_list(data['volatile']));
-          referer_div.after(volatile_referer_div);
-        }
-        referer_div.after(create_referer_list(data[date]));
+        button.after($(data));
         button.hide();
-        referer_div.show();
       }
     });
   });
-
-  referer_div.before(button);
-  referer_div.hide();
 });
--- plugin/lazy_referer.rb
+++ plugin/lazy_referer.rb
@@ -5,12 +5,8 @@
 # Copyright (C) 2013 by MATSUOKA Kohei <kmachu@gmail.com>
 # You can distribute it under GPL.
 #
-
 if @mode == 'day' and not bot? then
 	enable_js('referer.js')
-	add_js_setting('$tDiary.plugin.referer')
-	add_js_setting('$tDiary.plugin.referer.today', referer_today.to_json)
-	add_js_setting('$tDiary.plugin.referer.volatile', volatile_referer.to_json)
 
 	#
 	# overwrite method: draw only referer area (content will feach with ajax)
@@ -20,41 +16,17 @@ if @mode == 'day' and not bot? then
 		date = diary.date.strftime('%Y%m%d')
 		# FIXME: endpoint is should created by TDiary::Plugin, because easy customize routing
 		endpoint = "#{@conf.index}?plugin=referer&date=#{date}"
-		%Q[<div id="referer" data-date="#{h date}" data-endpoint="#{h endpoint}" class="caption">#{referer_today}</div>\n]
+		%Q[<button class="lazy_referer" style="padding: 0.5em; width: 20em;" data-endpoint="#{h endpoint}">#{referer_today}</button>\n]
 	end
-end
-
-def lazy_referer_to_array( diary )
-	array = []
-	# FIXME: referer_limit isn't correct (hard coding at skel)
-	limit = 100
-	diary.each_referer( limit ) do |count,href|
-		array.push({
-			:count => count,
-			:href => href,
-			:title => disp_referer( @conf.referer_table, href )
-		})
-	end
-	array.sort! {|a,b| b[:count] <=> a[:count] }
 end
 
 #
-# return referer of date as json
+# return referer of date as is (html)
 #
 add_content_proc('referer') do |date|
-	referer = { date => [], 'volatile' => [] }
-
 	diary = @diaries[date]
 	referer_load_current( diary )
-	referer[date] = lazy_referer_to_array( diary )
-
-	# TODO: return volatile referer at every diary?
-	if latest_day?( diary )
-		volatile = RefererDiary::new( @conf.latest_limit )
-		referer_load_volatile( volatile )
-		referer['volatile'] = lazy_referer_to_array ( volatile )
-	end
-	referer.to_json
+	referer_of_today_long( diary, 100 )
 end
 
 # Local Variables:
