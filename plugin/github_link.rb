56d8c6091808f9d86fa53e166ff1ccd10f0fe4dc
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2013-01-19 09:19:34 +0900

    gh_link allows to specify a text

--- plugin/github_link.rb
+++ plugin/github_link.rb
@@ -1,5 +1,5 @@
-def gh_link(gh_identifier)
-	text = gh_identifier.split('/').last
+def gh_link(gh_identifier, text=nil)
+	text = gh_identifier.split('/').last if text.nil?
 	id_and_name, number = gh_identifier.split('#')
 
 	url = "https://github.com/#{id_and_name}"
