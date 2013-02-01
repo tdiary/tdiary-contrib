cfab0956976f481cbc34cc17bc3e8e9445f74f66
MATSUOKA Kohei <kohei@machu.jp>
2013-02-02 07:29:37 +0900

    support AutoPagerize and AutoPatchWork

--- js/referer.js
+++ js/referer.js
@@ -6,19 +6,44 @@
  */
 
 $(function() {
-  var button = $('.lazy_referer');
-  var endpoint = button.data('endpoint');
+  
+  function lazy_referer(target) {
+    $('.lazy_referer', target).each(function () {
+      var button = $(this);
+      var endpoint = button.data('endpoint');
 
-  button.click(function() {
-    button.attr("disabled", "disabled");
-    $.ajax({
-      type: 'GET',
-      dataType: "html",
-      url: endpoint,
-      success: function(data) {
-        button.after($(data));
-        button.hide();
-      }
+      button.click(function() {
+        button.attr("disabled", "disabled");
+        $.ajax({
+          type: 'GET',
+          dataType: "html",
+          url: endpoint,
+          success: function(data) {
+            button.after($(data));
+            button.hide();
+          }
+        });
+      });
     });
+  }
+
+  // for AutoPagerize
+  $(window).bind('AutoPagerize_DOMNodeInserted', function(event) {
+    lazy_referer(event.target);
   });
+  
+  // for AuthPatchWork
+  // NOTE: jQuery.bind() cannot handle an event that include a dot charactor.
+  // see http://todayspython.blogspot.com/2011/06/autopager-autopagerize.html
+  if(window.addEventListener) {
+    window.addEventListener('AutoPatchWork.DOMNodeInserted', function(event) {
+      lazy_referer(event.target);
+    }, false);
+  } else if(window.attachEvent) {
+    window.attachEvent('onAutoPatchWork.DOMNodeInserted', function(event) {
+      lazy_referer(event.target);
+    });
+  };
+
+  lazy_referer(document);
 });
