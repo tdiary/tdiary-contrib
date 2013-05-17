18863a7d7fe5f89578736b8e96952cbeabd33636
MATSUOKA Kohei <kohei@machu.jp>
2013-05-18 07:00:31 +0900

    setup Sprokets environment

--- lib/tdiary-contrib.rb
+++ lib/tdiary-contrib.rb
@@ -1,4 +1,13 @@
 module TDiary
 	class Contrib
+		def self.root
+			File.expand_path('../..', __FILE__)
+		end
+
+		class Assets
+			def self.setup(environment)
+				environment.append_path File.join(TDiary::Contrib.root, 'js')
+			end
+		end
 	end
 end
