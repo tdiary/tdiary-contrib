240f9115abb4ea77fbfdefdfe641d6a3540ad3a0
MATSUOKA Kohei <kohei@machu.jp>
2013-06-16 16:29:16 +0900

    changing tDiary initialization process

--- lib/tdiary-contrib.rb
+++ lib/tdiary-contrib.rb
@@ -1,13 +1,10 @@
+require 'tdiary'
+require 'tdiary/application'
+
 module TDiary
 	class Contrib
 		def self.root
 			File.expand_path('../..', __FILE__)
-		end
-
-		class Assets
-			def self.setup(environment)
-				environment.append_path File.join(TDiary::Contrib.root, 'js')
-			end
 		end
 
 		class Plugin
@@ -15,5 +12,9 @@ module TDiary
 				sp_path << File.join(TDiary::Contrib.root, 'plugin')
 			end
 		end
+	end
+
+	Application.configure do
+		config.assets_paths << File.join(TDiary::Contrib.root, 'js')
 	end
 end
