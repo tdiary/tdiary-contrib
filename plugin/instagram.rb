d33601d605d3fb182d2a4a7e98f6b1c79a5264c5
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2013-07-11 12:36:20 +0900

    Create a plugin to embed instagram's photo/video

---
+++ plugin/instagram.rb
@@ -0,0 +1,10 @@
+# instagram.rb - embed your photo/videos in instagram to a diary
+#
+# Author: Tatsuya Sato 
+# License: MIT
+
+def instagram(code, width=612, height=700)
+  <<-BODY
+<iframe src="//instagram.com/p/#{code}/embed/" width="#{width}" height="#{height}" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+  BODY
+end
