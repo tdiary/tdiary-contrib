97a299cc89db83cbb2664e65d055a421472a60ec
MATSUOKA Kohei <kohei@machu.jp>
2013-12-10 19:52:14 +0900

    Revert "changing tDiary initialization process"
    This reverts commit 240f9115abb4ea77fbfdefdfe641d6a3540ad3a0.

--- lib/tdiary-contrib.rb
+++ lib/tdiary-contrib.rb
@@ -1,10 +1,13 @@
-require 'tdiary'
-require 'tdiary/application'
-
 module TDiary
 	class Contrib
 		def self.root
 			File.expand_path('../..', __FILE__)
+		end
+
+		class Assets
+			def self.setup(environment)
+				environment.append_path File.join(TDiary::Contrib.root, 'js')
+			end
 		end
 
 		class Plugin
@@ -12,9 +15,5 @@ module TDiary
 				sp_path << File.join(TDiary::Contrib.root, 'plugin')
 			end
 		end
-	end
-
-	Application.configure do
-		config.assets_paths << File.join(TDiary::Contrib.root, 'js')
 	end
 end
