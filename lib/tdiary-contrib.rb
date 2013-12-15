95d4b580663680834b673fd12aae3375e0014b9f
MATSUOKA Kohei <kohei@machu.jp>
2013-12-15 11:14:11 +0900

    define customize paths in TDiary::Extensions

--- lib/tdiary-contrib.rb
+++ lib/tdiary-contrib.rb
@@ -1,19 +1,10 @@
+# -*- coding: utf-8 -*-
+require 'extensions/contrib'
+
 module TDiary
 	class Contrib
 		def self.root
 			File.expand_path('../..', __FILE__)
-		end
-
-		class Assets
-			def self.setup(environment)
-				environment.append_path File.join(TDiary::Contrib.root, 'js')
-			end
-		end
-
-		class Plugin
-			def self.setup(sp_path)
-				sp_path << File.join(TDiary::Contrib.root, 'plugin')
-			end
 		end
 	end
 end
