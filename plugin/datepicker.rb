c88e514bb41038d58c7e537ac51e066ba04dbb17
TADA Tadashi <t@tdtds.jp>
2015-02-02 15:35:28 +0900

    support https access

--- plugin/datepicker.rb
+++ plugin/datepicker.rb
@@ -12,9 +12,9 @@
 return if feed? || @conf.mobile_agent?
 
 if /\A(?:form|preview|append|edit|update)\z/ =~ @mode
-   enable_js('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js')
+   enable_js('//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js')
    if @conf.lang == 'ja'
-      enable_js('http://ajax.googleapis.com/ajax/libs/jqueryui/1/i18n/jquery.ui.datepicker-ja.min.js')
+      enable_js('//ajax.googleapis.com/ajax/libs/jqueryui/1/i18n/jquery.ui.datepicker-ja.min.js')
    end
    enable_js('datepicker.js')
 end
