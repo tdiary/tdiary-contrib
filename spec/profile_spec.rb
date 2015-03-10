71c266a2ec4fbdb3e453ee033a8bb2b8558e60d3
TADA Tadashi <t@tdtds.jp>
2015-03-10 17:21:54 +0900

    support gravatar of full spec in profile plugin

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -8,6 +8,7 @@ describe "Profile::Service" do
     before do
       require 'json'
       allow_any_instance_of(Profile::Service::GitHub).to receive(:fetch).and_return(JSON.parse(File.read("spec/fixtures/github.json")))
+      allow_any_instance_of(Profile::Service::Gravatar).to receive(:fetch).and_return(JSON.parse(File.read("spec/fixtures/gravatar.json")))
 
       # workaround for run spec on various environment.
       require 'openssl'
@@ -20,7 +21,7 @@ describe "Profile::Service" do
     it "should include name, mail, image properties" do
       expect(@profile.name).to eq("Scott Chacon")
       expect(@profile.mail).to eq("schacon@gmail.com")
-      expect(@profile.image).to eq("http://www.gravatar.com/avatar/9375a9529679f1b42b567a640d775e7d.jpg?s=40")
+      expect(@profile.image).to eq("http://2.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?s=40")
     end
   end
 
@@ -55,14 +56,18 @@ describe "Profile::Service" do
   end
 
   describe "Gravatar" do
-    # http://ja.gravatar.com/site/implement/url
+    # http://ja.gravatar.com/site/implement/hash/
 
     before do
+      require 'json'
+      allow_any_instance_of(Profile::Service::Gravatar).to receive(:fetch).and_return(JSON.parse(File.read("spec/fixtures/gravatar.json")))
+
       @profile = Profile::Service::Gravatar.new("iHaveAn@email.com")
     end
-
-    it "should include image property" do
-      expect(@profile.image).to eq("http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802.jpg")
+    it "should include name, mail, image properties" do
+      expect(@profile.name).to eq("tDiary")
+      expect(@profile.mail).to eq("iHaveAn@email.com")
+      expect(@profile.image).to eq("http://2.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802")
     end
 
     context 'with options' do
@@ -71,7 +76,7 @@ describe "Profile::Service" do
       end
 
       it "should specify size option" do
-        expect(@profile.image).to eq("http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802.jpg?s=40")
+        expect(@profile.image).to eq("http://2.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?s=40")
       end
     end
   end
