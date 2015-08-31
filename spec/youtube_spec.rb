53032c849c7f366545890ab7d3c1a58e54fdae49
TADA Tadashi <t@tdtds.jp>
2015-08-31 17:28:43 +0900

    remove testing of user_agent 'iPhone' from youtube_spec

--- spec/youtube_spec.rb
+++ spec/youtube_spec.rb
@@ -5,7 +5,6 @@ describe "youtube plugin" do
 	DUMMY_YOUTUBE_VIDEO_ID = 1234567890
 
   {
-    'iPhone' => %|\t\t<iframe class="youtube-player" type="text/html" width="240" height="194" src="http://www.youtube.com/embed/#{DUMMY_YOUTUBE_VIDEO_ID}" frameborder="0">\n\t\t</iframe>\n\t\t<div class="youtube"><a href="http://www.youtube.com/watch?v=#{DUMMY_YOUTUBE_VIDEO_ID}">YouTube (#{DUMMY_YOUTUBE_VIDEO_ID})</a></div>\n|,
     'Mozilla' => %|\t\t<iframe class="youtube-player" type="text/html" width="425" height="350" src="http://www.youtube.com/embed/#{DUMMY_YOUTUBE_VIDEO_ID}" frameborder="0">\n\t\t</iframe>\n|
   }.each do |k,v|
     it 'should render object tag in :user_agent' do
