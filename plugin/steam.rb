6b04ab4c78a48c2f3c1a2c97d4c4b4b469a8be35
kp <knomura.1394@gmail.com>
2016-05-23 09:13:28 +0900

    set max-width simply.

--- plugin/steam.rb
+++ plugin/steam.rb
@@ -17,38 +17,5 @@
 =end
 
 def steam( id )
-   <<-HTML
-   <div class="steam-wrapper">
-      <div class="steam-container">
-         <iframe src="//store.steampowered.com/widget/#{id}/" frameborder="0" width="646" height="190"></iframe>
-      </div>
-   </div>
-   HTML
-end
-
-add_header_proc do
-   if @mode !~ /conf$/ and not bot? then
-      <<-HTML
-         <style type="text/css"><!--
-            div.steam-container{
-               position: relative;
-               padding-bottom:30%;
-               padding-top:100px;
-               height:0px;
-               overflow:hidden;
-            }
-            div.steam-container iframe{
-               position:absolute;
-               top:0;
-               left:0;
-               width:100%;
-               height:100%;
-            }
-
-            div.steam-wrapper{
-               width:650px;
-               max-width:100%;
-         --></style>
-      HTML
-   end
+   %Q[<iframe src="//store.steampowered.com/widget/#{id}/" frameborder="0" width="646" height="190" style="max-width:100%;"></iframe>]
 end
