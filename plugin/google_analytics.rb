b3a97d6944b678ba3d85c7bef1d8922003c64743
MATSUOKA Kohei <kohei@machu.jp>
2017-03-02 14:17:04 +0900

    google_analytics.rb: support AMP page

--- plugin/google_analytics.rb
+++ plugin/google_analytics.rb
@@ -29,10 +29,44 @@ end
 add_conf_proc( 'google_analytics', 'Google Analytics' ) do
 	if @mode == 'saveconf' then
 		@conf['google_analytics.profile'] = @cgi.params['google_analytics.profile'][0]
+		@conf['google_analytics.amp.profile'] = @cgi.params['google_analytics.amp.profile'][0]
 	end
-	<<-HTML
+	r = <<-HTML
 		<h3>Google Analytics Profile</h3>
 		<p>set your Profile ID (NNNNN-N)</p>
 		<p><input name="google_analytics.profile" value="#{h @conf['google_analytics.profile']}"></p>
 	HTML
+	if defined? AMP
+		r << <<-HTML
+			<h3>Google Analytics Profile for AMP page</h3>
+			<p>set your Profile ID (NNNNN-N) for AMP page</p>
+			<p><input name="google_analytics.amp.profile" value="#{h @conf['google_analytics.amp.profile']}"></p>
+		HTML
+	end
+	r
+end
+
+if defined? AMP
+	add_amp_header_proc do
+		%Q|<script async custom-element="amp-analytics"
+			src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>|
+	end
+
+	add_amp_body_enter_proc do
+		profile_id = %w(google_analytics.amp.profile google_analytics.profile).map {|key|
+			@conf[key]
+		}.find {|profile|
+			profile && !profile.empty?
+		}
+		<<-HTML
+			<amp-analytics type="googleanalytics" id="analytics1">
+			<script type="application/json">
+			{
+				"vars": { "account": "UA-#{h profile_id}" },
+				"triggers": { "trackPageview": { "on": "visible", "request": "pageview" } }
+			}
+			</script>
+			</amp-analytics>
+		HTML
+	end
 end
