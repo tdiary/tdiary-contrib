66e8638cc0e3c3c292898d8414f93e64b7979ef7
Tada, Tadashi <t@tdtds.jp>
2018-06-21 16:47:52 +0900

    change twitpic plugin's embed images to links
    * Twitpic was bought by Twitter
    * the thumbnail feature has been stopped by twitpic

--- plugin/twitpic.rb
+++ plugin/twitpic.rb
@@ -5,6 +5,6 @@
 # You can redistribute it and/or modify it under GPL2.
 #
 
-def twitpic( image_id, label = 'image on Twitpic', place = 'photo' )
-	%Q|<a class="twitpic" href="http://twitpic.com/#{h image_id}" title="#{h label}"><img class="#{h place}" src="http://twitpic.com/show/thumb/#{h image_id}.jpg" width="150" height="150" alt="#{h label}"></a>|
+def twitpic(image_id, label = 'Image on Twitpic', place = 'photo')
+	%Q|<p><a class="twitpic" href="http://twitpic.com/#{h image_id}">#{h label}</a></p>|
 end
