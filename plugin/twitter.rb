96a71fe622469c1145da543d539ec885b2f85b7b
Tada, Tadashi <t@tdtds.jp>
2020-06-24 11:01:26 +0900

    deprecate twitter and twitter_js plugins (#228)
    * remove all old code
    * show warning only in preview mode
    * remove spec of twitter_js

--- plugin/twitter.rb
+++ plugin/twitter.rb
@@ -1,45 +1,9 @@
-# twitter.rb $Revision: 1.1 $
-# Copyright (C) 2007 Michitaka Ohno <elpeo@mars.dti.ne.jp>
-# You can redistribute it and/or modify it under GPL2.
-
-require 'timeout'
-require 'time'
-require 'open-uri'
-require 'rexml/document'
-
-@twitter_statuses = []
-
-if /^(latest|day)$/ =~ @mode then
-	add_header_proc do
-		xml = nil
-		Timeout.timeout( 5 ) do
-			begin
-				xml = open( "http://twitter.com/statuses/user_timeline/#{@conf['twitter.user']}.xml" ){|f| f.read}
-			rescue Exception
-			end
-		end
-		doc = REXML::Document.new( xml ).root if xml
-		if doc then
-			doc.elements.each( 'status' ) do |e|
-				@twitter_statuses << [@conf.to_native( e.elements['text'].text ), Time.parse( e.elements['created_at'].text ).localtime]
-			end
-		end
-		''
-	end
-end
-
+#
+# twitter plugin is deprecate
+#
 add_body_leave_proc do |date|
-	today_statuses = []
-	@twitter_statuses.each do |t, d|
-		 today_statuses << [t, d] if d.to_a[3,3] == date.to_a[3,3]
-	end
-	if !today_statuses.empty?
-		r = %Q[<div class="section">]
-		r << %Q[<h3><a href="http://twitter.com/#{@conf['twitter.user']}">Twitter statuses</a></h3>]
-		today_statuses.sort{|a, b| b.last<=>a.last}.each do |t, d|
-			r << %Q[<p><strong>#{CGI::escapeHTML( t )}</strong> (#{d.strftime( '%H:%M:%S' )})</p>]
-		end
-		r << %Q[</div>]
+	if @mode == 'preview'
+		%Q[<p class="message">twitter plugin was deprecated.</p>]
 	else
 		''
 	end
--- plugin/twitter_js.rb
+++ plugin/twitter_js.rb
@@ -1,59 +1,10 @@
-# twitter_js.rb $Revision: 1.1 $
-# Copyright (C) 2007 Michitaka Ohno <elpeo@mars.dti.ne.jp>
-# You can redistribute it and/or modify it under GPL2.
-
-if /\A(?:latest|day)\z/ =~ @mode then
-	if @conf['twitter.user'] then
-		twitter_user = @conf['twitter.user']
-		add_header_proc do
-			result = <<-HTML
-			<script type="text/javascript"><!--
-			function twitter_cb(a){
-				var f=function(n){return (n<10?'0':'')+n};
-				for(var i=0,l=a.length;i<l;i++){
-					var d=new Date(a[i]['created_at'].replace('+0000','UTC'));
-					var id='twitter_statuses_'+f(d.getFullYear())+f(d.getMonth()+1)+f(d.getDate());
-					var e=document.getElementById(id);
-					if(!e) continue;
-					if(!e.innerHTML) e.innerHTML='<h3><a href="http://twitter.com/#{h twitter_user}">Twitter statuses</a></h3>';
-					e.innerHTML+='<p><strong>'+a[i]['text']+'</strong> ('+f(d.getHours())+':'+f(d.getMinutes())+':'+f(d.getSeconds())+')</p>';
-				}
-			}
-			function twitter_js(){
-				var e=document.createElement('script');
-				e.type='text/javascript';
-				e.src='http://twitter.com/statuses/user_timeline/#{h twitter_user}.json?callback=twitter_cb&amp;count=20';
-				document.documentElement.appendChild(e);
-			}
-			if(window.addEventListener){
-				window.addEventListener('load',twitter_js,false);
-			}else if(window.attachEvent){
-				window.attachEvent('onload',twitter_js);
-			}else{
-				window.onload=twitter_js;
-			}
-			// --></script>
-			HTML
-			result.gsub( /^\t\t/, '' )
-		end
-
-		add_body_leave_proc do |date|
-			result = <<-HTML
-			<div id="twitter_statuses_#{date.strftime( "%Y%m%d" )}" class="section"></div>
-			HTML
-			result.gsub( /^\t\t/, '' )
-		end
+#
+# twitter_js plugin is deprecate
+#
+add_body_leave_proc do |date|
+	if @mode == 'preview'
+		%Q[<p class="message">twitter_js plugin was deprecated.</p>]
+	else
+		''
 	end
-end
-
-add_conf_proc( 'twitter_js', 'Twitter' ) do
-
-	if @mode == 'saveconf' then
-		@conf['twitter.user'] = @cgi.params['twitter.user'][0]
-	end
-
-	<<-HTML
-   <h3 class="subtitle">Account Name</h3>
-   <p><input name="twitter.user" value="#{h @conf['twitter.user']}"></p>
-	HTML
 end
