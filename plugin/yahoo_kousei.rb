82689076d343218d3b8d45e54f78eca69b63936e
Takuya Ono <takuya-o@users.osdn.me>
2023-05-15 01:15:14 +0900

    🚚 chore(yahoo_kousei.rb): Yahooの校正支援のAPI V2対応
    従来利用していたV1 APIは既に提供が終わっているので
    とりあえずV2 APIに対応して結果が表示されるようにしました。

--- plugin/yahoo_kousei.rb
+++ plugin/yahoo_kousei.rb
@@ -5,37 +5,41 @@
 #  わかりにくい表記や不適切な表現が使われていないかなどをチェックします。
 #
 # Copyright (c) 2010, hb <http://www.smallstyle.com/>
+# Copyright (c) 2023, Takuya Ono <takuya-o@users.osdn.me>
 # You can redistribute it and/or modify it under GPL.
 #
 # 設定:
 #
 # @options['yahoo_kousei.appid'] : アプリケーションID(必須)
-# @options['yahoo_kousei.filter_group'] :
-#  指摘グループの番号をコンマで区切って指定します。
-# @options['yahoo_kousei.no_filter'] :
-#  filter_groupで指定した指摘グループから除外する指摘を指定します。
 #
-# 設定値は http://developer.yahoo.co.jp/webapi/jlp/kousei/v1/kousei.html を参照
+# 設定値は https://developer.yahoo.co.jp/webapi/jlp/kousei/v2/kousei.html を参照
 #
 
 require 'timeout'
-require 'rexml/document'
+require 'json'
 require 'net/http'
+require 'net/https'
 Net::HTTP.version_1_2
 
 def kousei_api( sentence )
 	appid = @conf['yahoo_kousei.appid']
+        headers = {'Content-Type' => 'application/json',
+                   'User-Agent' => "Yahoo AppID: #{appid}" }
 
-	query = "appid=#{appid}&sentence=#{CGI.escape( sentence.gsub( /\n/, '' ) )}"
-	query << "&filter_group=" + @conf['yahoo_kousei.filter_group'] if @conf['yahoo_kousei.filter_group']
-	query << "&no_filter=" + @conf['yahoo_kousei.no_filter'] if @conf['yahoo_kousei.no_filter']
+        query = { "id" => 1234,
+                  "jsonrpc" => "2.0",
+                  "method" => "jlp.kouseiservice.kousei",
+                  "params" => {
+                    "q" => sentence
+                  }
+                }
 
 	px_host, px_port = (@conf['proxy'] || '').split( /:/ )
 	px_port = 80 if px_host and !px_port
 
 	xml = ''
-	Net::HTTP::Proxy( px_host, px_port ).start( 'jlp.yahooapis.jp' ) do |http|
-		xml = http.post( '/KouseiService/V1/kousei', query ).body
+	Net::HTTP::Proxy( px_host, px_port ).start( 'jlp.yahooapis.jp',443, use_ssl: true , verify_mode: OpenSSL::SSL::VERIFY_PEER  ) do |http|
+		xml = http.post( '/KouseiService/V2/kousei', query.to_json, headers ).body
 	end
 	xml
 end
@@ -45,15 +49,24 @@ def kousei_result( result_set )
 	<h3>文章校正結果</h3>
 	HTML
 
-	doc = REXML::Document::new( result_set )
-	results = REXML::XPath.match( doc, "//Result" )
-	if results.empty?
-		html << "<p>指摘項目は見つかりませんでした。</p>"
+	doc = JSON.parse( result_set )
+        errormsg = ""
+        if doc["Error"] != nil
+          errormsg = doc["Error"]["Message"]
+          doc = { "result" => { "suggestions" => [] } }
+        end
+	results = doc['result']['suggestions']
+	if results.length == 0
+          if errormsg == ""
+	    html << "<p>指摘項目は見つかりませんでした。</p>"
+          else
+            html << "<p>Error: #{errormsg}</p>"
+          end
 	else
 		html << '<table>'
 		html << '<tr><th>対象表記</th><th>候補文字</th><th>詳細情報</th><th>場所</th></tr>'
-		doc.elements.each( '//ResultSet/Result' ) do |r|
-			html << %Q|<tr class="plugin_yahoo_search_result_raw"><td>#{r.elements['Surface'].text}</td><td>#{r.elements['ShitekiWord'].text}</td><td>#{r.elements['ShitekiInfo'].text}</td><td>#{r.elements['StartPos'].text},#{r.elements['Length'].text}</td></tr>|
+		results.each do |r|
+			html << %Q|<tr class="plugin_yahoo_search_result_raw"><td>#{r['word']}</td><td>#{r['suggestion']}</td><td>#{r['rule']} #{r['note']}</td><td>#{r['offset']},#{r['length']}</td></tr>|
 		end
 		html << '</table>'
 	end
